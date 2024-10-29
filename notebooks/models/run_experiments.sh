eee4e458962089ab5731e5e02432bae0067e30b0
root <jeremie.cabessa@gmail.com>
2024-10-29 08:42:58 +0000

    Run new exp on 10 epochs

--- notebooks/models/run_experiments.sh
+++ notebooks/models/run_experiments.sh
@@ -3,12 +3,12 @@
 export TQDM_DISABLE=1
 
 # "DeepChem/ChemBERTa-5M-MTR" "ncfrey/ChemGPT-4.7M"
-models=("ncfrey/ChemGPT-4.7M") # "DeepChem/ChemBERTa-5M-MTR"
+models=("DeepChem/ChemBERTa-5M-MTR") # "DeepChem/ChemBERTa-5M-MTR"
 
-data="exp"
-nb_epochs=5
+data="comp"  # exp remains XXX
+nb_epochs=10 # 5 epochs are too few
 
-hidden_layers=(3 5) # (0 1 3 5)
+hidden_layers=(5) # (0 1 3 5)
 
 # Loop
 for m in "${models[@]}"; do
@@ -16,14 +16,14 @@ for m in "${models[@]}"; do
 
         echo "Running experiment with model=$m, hidden_layers=$h, and epochs=$nb_epochs"
 
-        TRANSFORMERS_NO_PROGRESS_BAR=true python3 train_model.py \
+        TRANSFORMERS_NO_PROGRESS_BAR=true python train_model.py \
             --inputs smiles \
             --data_type "$data" \
             --model "$m" \
             --hidden_layers "$h" \
             --input_dim 200 \
             --hidden_dim 2200 \
-            --epochs "$nb_epochs" >> "experiments_${m#*/}_exp.log" 2>&1
+            --epochs "$nb_epochs" >> "experiments_${m#*/}_${data#*/}.log" 2>&1
 
         echo "Experiment completed."
         echo

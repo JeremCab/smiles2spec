82f290c0bf042cbd4bbcf0cb1de1c4640054d230
root <jeremie.cabessa@gmail.com>
2025-02-23 15:56:35 +0000

    Run new experiments

--- notebooks/models/run_experiments.sh
+++ notebooks/models/run_experiments.sh
@@ -7,7 +7,7 @@ export TQDM_DISABLE=1
 # "DeepChem/ChemBERTa-5M-MTR" "ncfrey/ChemGPT-4.7M"
 models=("DeepChem/ChemBERTa-5M-MTR") # "ncfrey/ChemGPT-4.7M" "DeepChem/ChemBERTa-5M-MTR"
 
-data=("comp" "exp" "comp_exp")            # "comp" "exp" "comp_exp" do comp and exp separately?
+data=("exp" "comp_exp")            # "comp" "exp" "comp_exp" do comp and exp separately?
 nb_epochs=16             # 15 originally; 16 if finetuning mode
 hidden_layers=(0 1 3)    # (0 1 3 5)
 
